4f1aa308c570f177e2e0f18ebb83e25827509518
Lucia Alderete <lu.4ld3r3t3@gmail.com>
2026-04-03 04:16:56 -0300

    ejercicio 3 completo

--- Punto_2/codigo_misterio.c
+++ Punto_2/codigo_misterio.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void f_alpha(int *p) {
+//la funcion invierte el numero inicial
+void invertido(int *p) {
     int temp = *p;
     int rev = 0;
     while (temp > 0) {
@@ -10,11 +11,12 @@ void f_alpha(int *p) {
     *p = rev;
 }
 
-void f_beta(int *p) {
+//al numero invertido lo divide en 2
+void mitadInvertido(int *p) {
     *p = *p / 2;
 }
-
-void f_gamma(int *p) {
+//suma el valor acutual con sus digitos 
+void sumaDigitos(int *p) {
     int temp = *p;
     int suma = 0;
     while (temp > 0) {
@@ -25,9 +27,9 @@ void f_gamma(int *p) {
 }
 
 void procesar_enigma(int *valor_referencia) {
-    f_alpha(valor_referencia);
-    f_beta(valor_referencia);
-    f_gamma(valor_referencia);
+    invertido(valor_referencia);
+    mitadInvertido(valor_referencia);
+    sumaDigitos(valor_referencia);
 }
 
 int main() {
